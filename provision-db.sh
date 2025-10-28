489e4ca3040f8af3de44c284b79be302f0837105
AndresGonzalez262 <andres_f.gonzalez_o@uao.edu.co>
2025-10-28 11:45:40 -0500

    Agrega index.html personalizado e info.php con mensaje dinámico

--- provision-db.sh
+++ provision-db.sh
@@ -1,1 +1,19 @@
 #debes completar este archivo con los comandos necesarios para provisionar la base de datos
+#!/bin/bash
+
+# Actualiza los paquetes
+sudo apt-get update
+
+# Instala PostgreSQL
+sudo apt-get install -y postgresql postgresql-contrib
+
+# Crear base de datos y tabla
+sudo -u postgres psql <<EOF
+CREATE DATABASE taller;
+\c taller
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(50)
+);
+INSERT INTO usuarios (nombre) VALUES ('Paola'), ('Andrea'), ('Ortegon');
+EOF
--- www/info.php
+++ www/info.php
@@ -1,4 +1,22 @@
 <?php
   echo "<h1>Hola desde PHP</h1>";
+  echo "<h2>Bienvenido</h2>";
+  echo "<p>Hoy es " . date("d/m/Y") . "</p>";
   phpinfo();
+?>
+
+<?php
+$conn = pg_connect("host=192.168.56.11 dbname=taller user=postgres");
+
+if (!$conn) {
+    echo "Error al conectar con la base de datos.";
+    exit;
+}
+
+$result = pg_query($conn, "SELECT * FROM usuarios");
+
+echo "<h1>Usuarios registrados</h1>";
+while ($row = pg_fetch_assoc($result)) {
+    echo "<p>ID: " . $row['id'] . " - Nombre: " . $row['nombre'] . "</p>";
+}
 ?>
